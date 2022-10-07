36b56eb40ed267c8d231e0484a776f2803ca16d1
Firebottle <justin@justingarver.com>
2022-10-07 13:10:36 -0500

    feat: Add building levels to !rpg world

--- src/firebot/commands/rpg-world.ts
+++ src/firebot/commands/rpg-world.ts
@@ -3,7 +3,10 @@ import {
     getWorldName,
     getWorldType,
 } from '../../systems/settings';
-import { verifyWorld } from '../../systems/world/world-stats';
+import {
+    getWorldBuildings,
+    verifyWorld,
+} from '../../systems/world/world-stats';
 import { logger, sendChatMessage, getWorldMeta } from '../firebot';
 
 export async function worldCommand() {
@@ -16,12 +19,19 @@ export async function worldCommand() {
     const worldCitizens = getWorldCitizens();
     const worldStats = await getWorldMeta();
 
+    const buildings = await getWorldBuildings();
+
     const message = `The ${worldType} of ${worldName} currently has ${
         worldStats.resources
     } resources, ${
         worldStats.research
     } research, and the ${worldCitizens.toLowerCase()} are ${
         worldStats.happiness
-    }% happy.`;
+    }% happy. | Tavern: ${buildings.tavern} | Blacksmith: ${
+        buildings.blacksmith
+    } | Enchanter: ${buildings.enchanter} | Shipyard: ${
+        buildings.shipyard
+    } | Guild: ${buildings.guild} | Trainer: ${buildings.trainer}`;
+
     sendChatMessage(message);
 }
